56fadde5401a16d83aa79fa13e5a60a1beb4ef73
chrysaldis <chrysaldis01@gmail.com>
2012-10-17 21:54:21 +0200

    Added new test

---
+++ src/test/java/com/mycompany/app/core/NewTest.java
@@ -0,0 +1,40 @@
+package com.mycompany.app;
+
+import junit.framework.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+     
+     @Before	
+     public void setUp() {
+     	//set up sth
+     }     
+
+     /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+    
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        int result = 2 - 2;	
+	assertTrue( result == 0 );
+    }
+	
+    @Test
+    public void shouldDoSth() {
+   	assert(true);	
+    }	
+
+}
